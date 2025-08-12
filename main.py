18f04e596a42bca8a8c55d6e52ec5d9817845929
Bishoy Sedra <bishosedra0@gmail.com>
2025-08-12 09:28:35 +0300

    Created the main file for the application with commented steps will be implemented!

---
+++ main.py
@@ -0,0 +1,11 @@
+# Step(1) Accepting the folder path from the user
+
+# Step(2) Scanning the folder for files not subdirectories
+
+# Step(3) Creating category subfolders
+
+# Step(4) Moving files to their respective category subfolders
+
+# Step(5) Showing summary of the files moved per category (Bonus)
+
+# Step(6) Adding --simulate option to test without moving files
